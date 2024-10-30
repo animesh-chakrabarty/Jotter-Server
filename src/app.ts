1eb18f6d4f593113c005fe86b5851cd7dda4cb18
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-10-30 21:45:27 +0530

    added CORS local

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
 import articleRouter from "./routes/article.routes.js";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 const createApp = () => {
   const app = express();
 
+  app.use(cors({ origin: "http://localhost:5173/" }));
   app.use(express.json());
 
   app.use("/api/auth", authRouter);
